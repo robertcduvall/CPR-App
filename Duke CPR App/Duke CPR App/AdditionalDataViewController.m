167d1c333b113588661bf06f1630bfb3a73c699c
Alexander Bruce <ajbruce60@gmail.com>
2012-10-17 15:33:52 -0400

    i think I'm going chinese

---
+++ Duke CPR App/Duke CPR App/AdditionalDataViewController.m
@@ -0,0 +1,54 @@
+#import "AdditionalDataViewController.h"
+#import <MediaPlayer/MediaPlayer.h>
+
+#define DUKE_HEALTH_URL @"http://www.dukehealth.org/health_library/video/compression-only-cpr-saves-lives"
+
+
+@interface AdditionalDataViewController ()
+@property (nonatomic, strong) MPMoviePlayerController *myVideoPlayer;
+@property (nonatomic, strong) UIView *myVideoView;
+@end
+
+@implementation AdditionalDataViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void) viewDidAppear:(BOOL)animated
+{
+    [self initializeVideoPlayer];
+}
+
+- (void) viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    [self.myVideoPlayer stop];
+}
+
+- (void) initializeVideoPlayer
+{
+    self.myVideoView = [[UIView alloc] initWithFrame:self.view.frame];
+    [self.view addSubview:self.myVideoView];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"CPR Instruction Video" ofType:@"mov"];
+    NSURL *videoURL = [NSURL fileURLWithPath:path];
+    
+    self.myVideoPlayer = [[MPMoviePlayerController alloc] initWithContentURL:videoURL];
+    self.myVideoPlayer.controlStyle = MPMovieControlStyleNone;
+    //[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(videoFinishedPlaying) name:MPMoviePlayerPlaybackDidFinishNotification object:self.myVideoPlayer];
+    self.myVideoPlayer.shouldAutoplay = YES;
+    [self.myVideoPlayer.view setFrame: self.myVideoView.bounds];  // player's frame must match parent's
+    [self.myVideoView addSubview: self.myVideoPlayer.view];
+    
+    //Play video
+    [self.myVideoPlayer prepareToPlay];
+    [self.myVideoPlayer play];
+}
+
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+}
+@end
---
+++ Duke CPR App/Duke CPR App/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Duke CPR App
+//
+//  Created by Alexander Bruce on 9/29/12.
+//  Copyright (c) 2012 Alexander Bruce. All rights reserved.
+//
+// Andrew was here!
+// I came after
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
